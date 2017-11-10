e182701cf80b2b8f860444d81d2975d6ad0da8be
RyougiCHan <ryougi.chan.fire@gmail.com>
2017-11-10 16:43:23 +0800

    Add rotate animation

--- FrontEnd/Scripts/index.js
+++ FrontEnd/Scripts/index.js
@@ -1,5 +1,19 @@
-(function() {
+(function () {
     'use strict';
+
+    $.fn.animateRotate = function (rotateType, startAngle, endAngle, duration, easing, complete) {
+        var args = $.speed(duration, easing, complete);
+        var step = args.step;
+        return this.each(function (i, e) {
+            args.complete = $.proxy(args.complete, e);
+            args.step = function (now) {
+                $.style(e, 'transform', rotateType + '(' + now + 'deg)');
+                if (step) return step.apply(e, arguments);
+            };
+
+            $({ deg: startAngle }).animate({ deg: endAngle }, args);
+        });
+    };
 
     /**
      * PC | Mobile layout switcher control
@@ -24,9 +38,9 @@
         var nav = document.querySelector(navSelector);
         var navLevel1 = nav.querySelector('ul');
         var navLevel1Items = navLevel1.children;
-        var hoverIn = function() {
+        var hoverIn = function () {
             var $this = $(this);
-            var show = function(speed) {
+            var show = function (speed) {
                 $this.children().css('display', 'block');
                 $this.children().css('opacity', 1);
                 $this.children().animate({
@@ -38,19 +52,19 @@
             if ($this.children().css('top') == '120px') {
                 show(300);
             }
-            setTimeout(function() {
+            setTimeout(function () {
                 if ($this.children().css('top') != '90px') {
                     show(0);
                 }
             }, 300);
         };
-        var hoverOut = function() {
+        var hoverOut = function () {
             var $this = $(this);
-            var hide = function(speed) {
+            var hide = function (speed) {
                 $this.children().animate({
                     top: '120px',
                     opacity: '0'
-                }, speed, function() {
+                }, speed, function () {
                     $this.children().css('display', 'none');
                 });
             };
@@ -58,7 +72,7 @@
             if ($this.children().css('top') == '90px') {
                 hide(300);
             }
-            setTimeout(function() {
+            setTimeout(function () {
                 if ($this.children().css('top') != '120px') {
                     hide(0);
                 }
@@ -68,23 +82,35 @@
     }
 
     // window resize control
-    $(window).on('resize', function(){
-        styleControl();    
+    $(window).on('resize', function () {
+        styleControl();
     });
     // Main container scroll control
-    $('#main').on('scroll', function(){
-        if(parseInt($('#main').scrollTop()) > 120) {
+    $('#main').on('scroll', function () {
+        if (parseInt($('#main').scrollTop()) > 120) {
             $('#main .main-to_top').fadeIn();
         }
-        if(parseInt($('#main').scrollTop()) <= 120) {
+        if (parseInt($('#main').scrollTop()) <= 120) {
             $('#main .main-to_top').fadeOut();
+        }
+        for (var i = 0; i < $('.main-content').length; i++) {
+            var temp = $('#main').scrollTop() - $('.main-content')[i].offsetTop + window.outerHeight - 120;
+            if (temp >= 0 && temp <= 20) {
+                $($('.main-content')[i]).animateRotate('rotateX', 90, 0, 400, 'swing');
+            }
         }
     });
     // Main to_top control
-    $('#main .main-to_top').on('click',function(){
+    $('#main .main-to_top').on('click', function () {
         $('#main').animate({
-            scrollTop:0
-        },300);
+            scrollTop: 0
+        }, 300, function () {
+            $('#main .main-to_top').css('display', 'none');
+            $('#main .main-to_top').css('top', 'auto');
+        });
+        $('#main .main-to_top').animate({
+            top: -100
+        }, 250);
     });
     styleControl();
     navGo('#header');
